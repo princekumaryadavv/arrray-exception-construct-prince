1ff9ab70cb26248f5634e0d4d4c6fad85cc3a011
princekumaryadavv <129711840+princekumaryadavv@users.noreply.github.com>
2023-04-02 21:50:41 -0700

    array

---
+++ src/array.java
@@ -0,0 +1,24 @@
+package first;
+
+import java.util.Scanner;
+
+public class array {
+
+	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("Enter your number");
+		int a,b,c;
+		try
+		{
+			a=scanner.nextInt();
+			b=scanner.nextInt();
+		c=a/b;
+		System.out.println(c);
+		}
+		catch (Exception e)
+		{
+			System.out.println("Please do proper input");
+		}
+	}
+
+}
